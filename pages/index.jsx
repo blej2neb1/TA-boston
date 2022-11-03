b90108ba0484d685560a97b4a03ede2dce11b63e
blej2neb1 <104223774+blej2neb1@users.noreply.github.com>
2022-11-03 22:02:46 +0200

    ta-3nov2022

--- pages/index.jsx
+++ pages/index.jsx
@@ -5,6 +5,12 @@ export default function Home() {
     <div>tred afer <i className="fa-solid fa-house"></i>
 
     <img src="/images/logo.jpg" alt="Alt"></img>
+
+    <button className="button button-primary">Button Primary</button>
+    <button className="button button-primary button-large">Button Primary Large</button>
+    <a className="button button-primary">Button Primary</a>
+    <a className="button button-primary button-large">Button Primary Large</a>
+
     </div>
   )
 }
